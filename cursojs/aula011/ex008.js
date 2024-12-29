82f316ba72a6a094ed8bfdb96735ce91afcc1ead
Vitória Freitas <vitoriafreitas110903@gmail.com>
2024-12-29 11:40:36 -0300

    atualizado

---
+++ cursojs/aula011/ex008.js
@@ -0,0 +1,6 @@
+var vel = 70
+console.log(`A velocidade do seu carro ${vel}km/h`)
+if (vel > 60) {
+    console.log(`Voce ultrapassou a velocidade permitida. MULTADO`)
+}
+console.log(`dirija sempre usando cinto de segurança`)

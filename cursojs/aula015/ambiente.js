904241f5723bed53ac3fd971051601b00a1fb619
Vitória Freitas <vitoriafreitas110903@gmail.com>
2025-01-16 12:13:17 -0300

    atualizado

---
+++ cursojs/aula015/ambiente.js
@@ -0,0 +1,12 @@
+let num = [5, 8, 2, 9, 3]
+num[5] = 4
+num.push(1)
+console.log(`(${num}) => O vetor tem ${num.length} posições`)
+console.log(`${num[0]}`)
+let pos = num.indexOf(9)
+if (pos == -1){
+    console.log('O valor não foi encontrado!')
+}else {
+    console.log(`o valor 8 esta na posição ${pos}`)
+}
+

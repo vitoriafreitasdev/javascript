16403da6ec1795d8f3fd915571f7abcc9c4599ac
Vitória Freitas <vitoriafreitas110903@gmail.com>
2025-01-16 10:17:10 -0300

    atualizei

--- cursojs/aula12ex/ex015/script.js
+++ cursojs/aula12ex/ex015/script.js
@@ -3,6 +3,9 @@ function verificar(){
     var ano = data.getFullYear()
     var fano = document.getElementById('txtano')
     var res = document.querySelector('div#res')
+
+    var fsex = document.getElementsByName('radsex')
+
     if (fano.value.length == 0 || fano.value > ano) {
         window.alert('[ERRO] Verifique os dados e tente novamente!') 
     } else {
--- cursojs/aula14ex/ex017/script.js
+++ cursojs/aula14ex/ex017/script.js
@@ -8,13 +8,23 @@
         let n = Number(num.value)
         let c = 1 
         tab.innerHTML = ''
-        while (c <= 10){
+        for (let c = 1; c <=10; c++){
             let item = document.createElement('option')
             let res = n*c
             item.text = `${n} x ${c} = ${res} `
             item.value = `tab${c}`
             tab.appendChild(item)
-            c++
         }
     }
  }
+
+
+
+    //while (c <= 10){
+       // let item = document.createElement('option')
+      //  let res = n*c
+     //   item.text = `${n} x ${c} = ${res} `
+     //   item.value = `tab${c}`
+     //   tab.appendChild(item)
+      //  c++
+   // }
